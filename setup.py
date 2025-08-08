4f06e342269c5c18637a644eb52fdd2a54f95133
maheshpachpande <pachpandemahesh300@gmail.com>
2025-08-08 18:27:58 +0530

    setup created with toml file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mahesh Pachpande",
+    author_email="pachpandemahesh300@gmail.com",
+    packages=find_packages()
+)
